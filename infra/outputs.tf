dc1abda4c65381a7216f0eb82c67721890a1fdae
holyshared <holy.shared.design@gmail.com>
2021-08-15 17:30:50 +0900

    add outputs parameters

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,8 +1,16 @@
-output "credentials" {
+output "dataflow_credentials" {
   value = base64decode(google_service_account_key.dataflow_key.private_key)
   sensitive = true
 }
 
-output "vpc_network_name" {
+output "dataflow_service_account_email" {
+  value = google_service_account.dataflow_service_account.email
+}
+
+output "dataflow_vpc_network_name" {
   value = google_compute_network.vpc_network.name
+}
+
+output "dataflow_bucket_name" {
+  value = google_storage_bucket.logs.name
 }
