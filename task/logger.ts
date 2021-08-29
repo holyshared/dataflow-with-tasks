3ef12269de6de0bc658013ccc0db7d9c9a782973
holyshared <holy.shared.design@gmail.com>
2021-08-29 16:57:04 +0900

    add logger name

--- task/logger.ts
+++ task/logger.ts
@@ -3,10 +3,11 @@ import { LoggingBunyan } from '@google-cloud/logging-bunyan'
 
 const loggingBunyan = new LoggingBunyan({
   projectId: process.env.GCP_PROJECT_ID,
-  logName: "dataflow",
-});
+  logName: 'dataflow',
+})
 
 export const logger = createLogger({
+  name: 'dataflow',
   streams: [
     loggingBunyan.stream('info'),
   ],
